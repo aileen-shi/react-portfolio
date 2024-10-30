2aabc028049cdb3e9460879a6f7caed829dda246
Aileen Shi <89747088+aileen-shi@users.noreply.github.com>
2024-10-30 08:43:27 -0400

    started navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,25 +1,33 @@
-import { Link, NavLink } from 'react-router-dom';
-import './index.scss';
+import { Link, NavLink } from 'react-router-dom'
+import './index.scss'
 
 const Navbar = () => {
-    return (
-       <div className='nav-bar'>
-            <Link to='/'>
-                <h1>aileen</h1>
-            </Link>
-            <nav>
-                <NavLink exact="true" activeclassname="active" to="/about">
-                    <button>about</button>
-                </NavLink>
-                <NavLink exact="true" activeclassname="active" to="/projects">
-                    <button></button>
-                </NavLink>
-                <NavLink exact="true" activeclassname="active" to="/contact">
-                    <button>about</button>
-                </NavLink>
-            </nav>
-       </div>
-    )
+  return (
+    <div className="nav-bar">
+      <Link to="/">
+        <h1>aileen</h1>
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <NavLink exact="true" activeclassname="active" to="/about">
+              <button>about</button>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact="true" activeclassname="active" to="/projects">
+              <button>projects</button>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact="true" activeclassname="active" to="/contact">
+              <button>contact</button>
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
 }
 
-export default Navbar;
+export default Navbar
