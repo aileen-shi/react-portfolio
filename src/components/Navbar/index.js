5c751762dc72003a3a75d45d2b93d70ac8b8f901
Aileen Shi <89747088+aileen-shi@users.noreply.github.com>
2024-10-31 18:10:56 -0400

    Styling hamburger menu progress

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,31 +1,39 @@
-import { Link, NavLink } from 'react-router-dom'
-import './index.scss'
+import { Link, NavLink } from 'react-router-dom';
+import './index.scss';
+import { ReactComponent as BurgerMenu } from '../../assets/icons/BurgerMenu.svg';
 
 const Navbar = () => {
   return (
     <div className="nav-bar">
-      <Link to="/">
-        <h1>aileen</h1>
-      </Link>
-      <nav>
-        <ul>
-          <li>
-            <NavLink exact="true" activeclassname="active" to="/about">
-              <button>about</button>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink exact="true" activeclassname="active" to="/projects">
-              <button>projects</button>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink exact="true" activeclassname="active" to="/contact">
-              <button>contact</button>
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+        <div className="nav-content">
+            <Link className="link" to="/">
+                <h1>aileen</h1>
+            </Link>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink exact="true" activeclassname="active" to="/about">
+                            <button id="about-button">about</button>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact="true" activeclassname="active" to="/projects">
+                            <button id="projects-button">projects</button>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact="true" activeclassname="active" to="/contact">
+                            <button id="contact-button">contact</button>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink>
+                            <BurgerMenu className="icon"></BurgerMenu>
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </div>
   )
 }
