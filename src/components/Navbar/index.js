61a5405630a55831149bbeea346be1be0afed709
Aileen Shi <89747088+aileen-shi@users.noreply.github.com>
2024-11-21 18:09:27 -0500

    Minor tweaks

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,41 +1,47 @@
 import { Link, NavLink } from 'react-router-dom';
 import './index.scss';
 import { ReactComponent as BurgerMenu } from '../../assets/icons/BurgerMenu.svg';
+import { useState } from "react";
 
 const Navbar = () => {
-  return (
-    <div className="nav-bar">
-        <div className="nav-content">
-            <Link className="link" to="/">
-                <h1>aileen</h1>
-            </Link>
-            <nav>
-                <ul>
-                    <li>
-                        <NavLink exact="true" activeclassname="active" to="/about">
-                            <button id="about-button">about</button>
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink exact="true" activeclassname="active" to="/projects">
-                            <button id="projects-button">projects</button>
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink exact="true" activeclassname="active" to="/contact">
-                            <button id="contact-button">contact</button>
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink>
-                            <BurgerMenu className="icon"></BurgerMenu>
-                        </NavLink>
-                    </li>
-                </ul>
-            </nav>
+    const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
+    const toggleHamburger = () => {
+        console.log("clicked");
+        setHamburgerOpen(!hamburgerOpen);
+    }
+
+    return (
+        <div className="nav-bar">
+            <div className="nav-content">
+                <Link className="link" to="/">
+                    <h1>aileen</h1>
+                </Link>
+                <nav>
+                    <ul>
+                        <li>
+                            <NavLink exact="true" activeclassname="active" to="/about">
+                                <button id="about-button">about</button>
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink exact="true" activeclassname="active" to="/projects">
+                                <button id="projects-button">projects</button>
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink exact="true" activeclassname="active" to="/contact">
+                                <button id="contact-button">contact</button>
+                            </NavLink>
+                        </li>
+                        <li>
+                            <BurgerMenu className="icon" id="burger-icon" onClick={toggleHamburger}></BurgerMenu>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
