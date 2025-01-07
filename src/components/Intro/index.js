99a2ee1391211e6ea99d223095ffdb3528780816
Aileen Shi <89747088+aileen-shi@users.noreply.github.com>
2025-01-07 16:27:34 -0500

    Updated profile
    Added profile image

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -1,22 +1,24 @@
-import './index.scss';
-import { ReactComponent as Dots } from '../../assets/icons/dotgrid.svg';
-import { ReactComponent as Arrow } from '../../assets/icons/arrow.svg';
+import './index.scss'
+import { ReactComponent as Dots } from '../../assets/icons/dotgrid.svg'
+import { ReactComponent as Arrow } from '../../assets/icons/arrow.svg'
+import portrait from '../../assets/images/me.png'
 
 const Intro = () => {
-    return (
-        <div className="intro">
-            <Dots className="dots" />
-            <div className="title-box">
-                <h1>hey!</h1>
-                <h1>i'm aileen</h1>
-                <h5>a full stack developer.</h5>
-            </div>
-            <div className="portrait-box">
-                <h1>testing</h1>
-            </div>
-            <Arrow className="arrow" />
-        </div>
-    )
+  return (
+    <div className="intro">
+      <Dots className="dots" />
+      <Arrow className="arrow" />
+
+      <div className="title-box">
+        <h1>hey!</h1>
+        <h1>i'm aileen</h1>
+        <h5>a full stack developer.</h5>
+      </div>
+      <div className="portrait-box">
+        <img className="portrait" src={portrait}></img>
+      </div>
+    </div>
+  )
 }
 
-export default Intro;
+export default Intro
